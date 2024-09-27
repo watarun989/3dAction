08cc64f8a90f55f2ac7f4216ae07236c8c61cd3b
Akio Mizukami <>
2024-09-27 17:34:21 +0400

    0927 Enemy going back to Start Point

--- Assets/Scripts/MoveTarget.cs
+++ Assets/Scripts/MoveTarget.cs
@@ -11,10 +11,12 @@ public class MoveTarget : MonoBehaviour
     //追う対象　変数名player
     public GameObject player; 
 
+    Vector3 enemyPosition; 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //エネミーの最初の位置を取得
+        enemyPosition = transform.position; 
     }
 
     // Update is called once per frame
@@ -26,6 +28,9 @@ public class MoveTarget : MonoBehaviour
             //（）に指定した座標に向かう （）の中は座標（Vector3型）
             //プレイヤーの座標に向かう
             agent.SetDestination(player.transform.position); 
+        } else{
+            //エネミーの初期地点へ向かう
+            agent.SetDestination(enemyPosition); 
         }
     }
 }
